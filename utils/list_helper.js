32f9207c82c88ae8bb3dad1455e15f103a021c0c
Mika Kärkkäinen <mika.karkkainen@koivuniemi.com>
2018-10-28 12:29:35 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
     return 1;
 };
 
+const totalLikes = blogs => {
+    //return Object.values(blogs).reduce((t, n) => t + n.likes, 0);
+    const reducer = (sum, item) => {
+        return sum + item.likes;
+    };
+    return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 };
