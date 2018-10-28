d821ef3f1d565bea788b693d4489e1f47f22cca1
Mika Kärkkäinen <mika.karkkainen@koivuniemi.com>
2018-10-28 13:30:59 +0200

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,27 @@ const totalLikes = blogs => {
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 0) {
+        return 0;
+    }
+    let max = blogs[0].likes;
+    let index = 0;
+    for (let i = 1, len = blogs.length; i < len; i++) {
+        if (blogs[i].likes > max) {
+            max = blogs[i].likes;
+            index = i;
+        }
+    }
+    return {
+        title: blogs[index].title,
+        author: blogs[index].author,
+        likes: blogs[index].likes
+    };
+};
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
